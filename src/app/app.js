3cc8a47e467568b2a3537bde8d17d7697f256fe2
Avinash Varma <avinashvarma@live.com>
2019-11-10 02:52:32 +0800

    Initial Commit.

---
+++ src/app/app.js
@@ -0,0 +1,1 @@
+var app = angular.module('tableApp', ['angularUtils.directives.dirPagination']);
---
+++ src/app/controller/tableCtrl.js
@@ -0,0 +1,50 @@
+app.controller("tableCtrl", function tableCtrl($scope) {
+    $scope.currentPage = 1;
+    $scope.pageSize = 10;
+    $scope.items = [];
+    for (var i = 0; i < 1000; i++) {
+        var idStr = (i < 10 ? "0" + i : i).toString();
+        var nameStr = String.fromCharCode(65 + i); //gets alphabet chars
+        var priceStr = (Math.random() * 100).toFixed(2);
+        var qty = Math.floor(Math.random() * (25 - 1 + 1) + 1);
+        $scope.items.push({
+            Id: idStr,
+            Name: nameStr + "vi",
+            Price: priceStr,
+            Info: {
+                Quantity: qty
+            },
+            Description: "This is the description for item " + nameStr + ".",
+            selected: false
+        });
+    }
+    $scope.sort = function(keyname){
+        $scope.sortKey = keyname;   //set the sortKey to the param passed
+        $scope.reverse = !$scope.reverse; //if true make it false and vice versa
+    }
+    $scope.noitems = [];
+
+    $scope.clickRow = function () {
+        alert('You clicked the row.');
+    }
+
+    function swapBeforeAndAfterDecimal(number) {
+        var beforeDecimal = Math.floor(number).toString();
+        var afterDecimal = number.toString().replace(/.*\./, '');
+
+        return afterDecimal + '.' + beforeDecimal;
+    }
+
+    $scope.centsThenDollars = function (a, b) {
+        var aSwapped = swapBeforeAndAfterDecimal(a);
+        var bSwapped = swapBeforeAndAfterDecimal(b);
+
+        if (aSwapped > bSwapped) {
+            return 1;
+        } else if (aSwapped === bSwapped) {
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+});
